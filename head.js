e9704ab81f767cb1976b096e13001c479347f027
Grant <grantimaxi03@gmail.com>
2024-03-19 01:47:02 -0400

    Head Function

---
+++ head.js
@@ -0,0 +1,18 @@
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+      console.log(`✔✔✔  Assertion Passed: ${actual} === ${expected}`);
+    } else {
+      console.log(`🧨🧨🧨 Assertion Failed: ${actual} !== ${expected}`);
+    }
+  };
+
+const head = function(arr) {
+    let first = arr[0];
+    return first;
+}
+
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head([]), "Hello");
+assertEqual(head([5,6,7]), 7);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Lighthouse");

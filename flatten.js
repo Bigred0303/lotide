fa767cbc72c0a65deed9644e81f8140d533305a9
Grant <grantimaxi03@gmail.com>
2024-03-24 22:34:04 -0400

    Created a function to flatten arrays

---
+++ flatten.js
@@ -0,0 +1,34 @@
+const flatten = function(arr) {
+  let flatArr = arr.reduce((accumulator, value) => accumulator.concat(value), []);
+  console.log(flatArr);
+};
+
+const eqArrays = function(arr1, arr2) {
+  let stillTrue = true;
+  let i = 0;
+  if (arr1 === undefined || arr2 === undefined) {
+    return stilltrue = false;
+  }
+  if (arr1.length === arr2.length) {
+    for (const element of arr1) {
+      if (element !== arr2[i])
+        stillTrue = false;
+      i++;
+    }
+  } else {
+    stillTrue = false;
+  }
+      
+  return stillTrue;
+};
+    
+const assertArraysEqual = function(arr1, arr2) {
+  let arrayCheck = eqArrays(arr1, arr2);
+  if (arrayCheck) {
+    console.log(`✔✔✔  Assertion Passed: ${arr1} === ${arr2} The arrays are equal`);
+  } else {
+    console.log(`🧨🧨🧨 Assertion Failed: ${arr1} !== ${arr2} The arrays aren't equal`);
+  }
+};
+
+flatten([1, 2, [3, 4], 5, [6]]);

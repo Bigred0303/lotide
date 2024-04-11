cdc04d8d0e1c9c9cfe9445ea65d2d2d47e122dbd
Grant <grantimaxi03@gmail.com>
2024-04-11 12:01:16 -0400

    Fixed a improperly copied line

--- test/tailTest.js
+++ test/tailTest.js
@@ -7,4 +7,4 @@ const assertEqual = require("../assertEqual");
 assertEqual(tail([1]).length, 0);
 assertEqual(tail([1, 2, 3, 4, 5]).length, 4);
 assertEqual(tail([1, 2, 3, 4]).length, 3);
-assertEqual(tail([]), 0);
+assertEqual(tail([]).length, 0);

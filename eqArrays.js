8bc3ef9ac590992b6d4134ccce7b32a7eb94ba19
Grant <grantimaxi03@gmail.com>
2024-04-11 12:06:19 -0400

    Refactored eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,3 @@
-
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✔✔✔  Assertion Passed: ${actual} === ${expected} The arrays are equal`);
-  } else {
-    console.log(`🧨🧨🧨 Assertion Failed: ${actual} !== ${expected} The arrays aren't equal`);
-  }
-};
-
 const eqArrays = function(arr1, arr2) {
   let stillTrue = true;
   let i = 0;
@@ -24,6 +14,4 @@ const eqArrays = function(arr1, arr2) {
   return stillTrue;
 };
 
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([], []), true);
+module.exports = eqArrays;

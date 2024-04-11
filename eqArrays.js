167ab7ebb20c767f18fb827c7f5bebca9b7e75df
Grant <grantimaxi03@gmail.com>
2024-04-11 12:10:27 -0400

    Fixed eqArrays up a bit

--- eqArrays.js
+++ eqArrays.js
@@ -1,16 +1,9 @@
 const eqArrays = function(arr1, arr2) {
   let stillTrue = true;
-  let i = 0;
-  if (arr1.length === arr2.length) {
-    for (const element of arr1) {
-        if (element !== arr2[i])
-          stillTrue = false;
-        i++;
-      }
-  } else {
-    stillTrue = false;
-  }
-
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i])
+      stillTrue = false;
+    }
   return stillTrue;
 };
 

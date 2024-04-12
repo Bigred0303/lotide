6d2478d3be1606660dea4208714bae16d3bf3c17
Grant <grantimaxi03@gmail.com>
2024-04-12 03:45:16 -0400

    M&C tail + linted head and tail

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,20 @@
 const tail = require("../tail");
-const assertEqual = require("../assertEqual");
+const assert = require('chai').assert;
+
+describe("#tail", () => {
+  it("returns [] for [1]", () => {
+    assert.deepEqual(tail([1]), []);
+  });
+  it("returns '[2, 3, 4, 5]' for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(tail([1, 2, 3, 4, 5]), [2, 3, 4, 5]);
+  });
+  it("returns '[]' for []", () => {
+    assert.deepEqual(tail([]), []);
+  });
+});
 
 
-
-
-assertEqual(tail([1]).length, 0);
-assertEqual(tail([1, 2, 3, 4, 5]).length, 4);
-assertEqual(tail([1, 2, 3, 4]).length, 3);
-assertEqual(tail([]).length, 0);
+// assertEqual(tail([1]).length, 0);
+// assertEqual(tail([1, 2, 3, 4, 5]).length, 4);
+// assertEqual(tail([1, 2, 3, 4]).length, 3);
+// assertEqual(tail([]).length, 0);
